04acdefc0c10f3fca076f3b6e80d1e1541e3e43e
Almir Menezes da Cunha Júnior <almirmdacunha@gmail.com>
2025-03-23 16:08:44 -0300

    refactor: update image generation prompt and streamline frame saving process in VisualService

--- api/visual/visual_service.py
+++ api/visual/visual_service.py
@@ -16,37 +16,35 @@ class VisualService:
         self.semaphore = asyncio.Semaphore(max_concurrent_requests)
 
     def _get_image_generation_prompt(self, frame: Frame) -> str:
-        return f'''
-        Generate an image for the following narrative:
-        {frame.narration}
+        return f'''Generate a scene for the following narrative:
+{frame.narration}
 
-        The detailed description of the image is:
-        {frame.description}
+The detailed description of the image is:
+{frame.description}
 
-        The image should be in anime style.
-        '''
+Make sure the characteres are acting in a way that is consistent with the narrative.
 
-    async def _generate_and_save_frame(self, frame: Frame, i: int, output_dir: str) -> str:
+The scenes must have a cartoon style.
+'''
+
+    async def _generate_and_save_frame(self, frame: Frame, image_file_path: str) -> str:
         try:
             async with self.semaphore:
                 base64_image = await self.tti.to_image(self._get_image_generation_prompt(frame))
-                
                 if not base64_image:
-                    raise ImageGenerationError(f"No image generated for frame {i+1}")
+                    raise ImageGenerationError("Empty response from TTI")
                 
-                file_path = os.path.join(output_dir, f"frame_{i+1}.png")
-                with open(file_path, "wb") as f:
+                with open(image_file_path, "wb") as f:
                     f.write(base64.b64decode(base64_image))
                 
-                return file_path
+                return image_file_path
         except Exception as e:
-            self.logger.error(f"Failed to generate/save image for frame {i+1}: {str(e)}")
-            raise ImageGenerationError(f"Failed to generate/save image for frame {i+1}: {str(e)}")
+            raise ImageGenerationError(f"Failed to generate image: {str(e)}")
 
     async def generate_frames(self, script: Script, output_dir: str) -> List[str]:
         async with aiohttp.ClientSession() as session:
             tasks = [
-                self._generate_and_save_frame(frame, i, output_dir)
+                self._generate_and_save_frame(frame, os.path.join(output_dir, f"frame_{i+1}.png"))
                 for i, frame in enumerate(script.frames)
             ]
             
@@ -54,4 +52,4 @@ class VisualService:
             return image_paths
 
 class InvalidChatBotResponse(Exception):
-    pass 
+    pass
